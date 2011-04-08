4f152a8986224d93b66c3326006735fcb0fb8de5
Paul Kelly <pkelly@packardbell.(none)>
2011-04-08 05:56:21 +0100

    With tables

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,12 @@
 module HomeHelper
-  def expand what
+  def shuffle what
     update_page do |page|
-      page.visual_effect(:toggle_blind, what)
+      for dom in what[:expand]
+        page.visual_effect(:blind_down, dom)
+      end
+      for dom in what[:contract]
+        page.visual_effect(:blind_up, dom)
+      end
     end
   end
 end
