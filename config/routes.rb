e0af54cb0e839cfb642d335a5a3450db68ae7e52
Paul Kelly <paul.ian.kelly@googlemail.com>
2011-04-25 11:00:20 +0100

    Added a popup window as an example
    Maybe you can mess with the window.open arguments to make this a true popup.
    I do not know

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => "home"
   map.resources :prices
 
-  map.resources :quotes, :collection => {:computer_changed => :get, :calculate => :post}
+  map.resources :quotes, :collection => {:computer_changed => :get, :calculate => :post, :how_to_find => :get}
 
   map.resources :grades
 
