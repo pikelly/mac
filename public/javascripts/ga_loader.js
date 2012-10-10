a4252cf908e5fb9fd29d755b5879e7c7ce9b67a7
Paul Kelly <paul.ian.kelly@googlemail.com>
2012-10-10 17:41:03 +0100

    Added google_analytics

---
+++ public/javascripts/ga_loader.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: pkelly
+ * Date: 04/10/12
+ * Time: 16:58
+ * To change this template use File | Settings | File Templates.
+ */
+var _gaq = _gaq || [];
+
+  _gaq.push(['_setAccount', 'UA-33783498-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
